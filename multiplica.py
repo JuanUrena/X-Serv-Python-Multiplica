46be4b2aacd61bca7ee08d98b61115f574413c30
Juan Urena Garcia Gasco <jurenag@beta13.aulas.gsyc.urjc.es>
2018-01-30 12:38:46 +0100

    Ejercicio resuelto

---
+++ multiplica.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Programa Tablas de Muñtiplicar
+
+numeros = list(range(1, 11))
+
+for num1 in numeros:
+    print('Tabla del' , num1)
+    for num2 in numeros:
+        print(num1, ' por ', num2, " es ", num2*num1)
+        
+    
